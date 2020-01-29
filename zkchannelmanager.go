5fe259a62cef8c0dc74da0d9a4a433a9125cb83d
Darius Parvin <darius@berkeley.edu>
2020-01-29 14:33:05 -0500

    move other message processing to zkchannelmanager

--- zkchannelmanager.go
+++ zkchannelmanager.go
@@ -12,11 +12,14 @@ type zkChannelManager struct {
 
 func (z *zkChannelManager) processZkPayProof(msg *lnwire.ZkPayProof, p lnpeer.Peer) {
 
+	zkchLog.Info("Just received ZkPayProof with length: ", len(msg.Payment))
+
 	// // To load from rpc message
 	var payment string
 	err := json.Unmarshal(msg.Payment, &payment)
 	_ = err
 
+	// TEMPORARY DUMMY MESSAGE
 	closeTokenBytes := []byte{'d', 'u', 'm', 'm', 'y', 'y', 'y', 'y'}
 
 	zkPayClose := lnwire.ZkPayClose{
@@ -24,4 +27,34 @@ func (z *zkChannelManager) processZkPayProof(msg *lnwire.ZkPayProof, p lnpeer.Pe
 	}
 	p.SendMessage(false, &zkPayClose)
 
+}
+
+func (z *zkChannelManager) processZkPayClose(msg *lnwire.ZkPayClose, p lnpeer.Peer) {
+
+	zkchLog.Info("Just received ZkPayClose with length: ", len(msg.CloseToken))
+
+	// TEMPORARY dummy message
+	revokeTokenBytes := []byte{'d', 'u', 'm', 'm', 'y'}
+
+	zkPayRevoke := lnwire.ZkPayRevoke{
+		RevokeToken: revokeTokenBytes,
+	}
+	p.SendMessage(false, &zkPayRevoke)
+
+}
+
+func (z *zkChannelManager) processZkPayRevoke(msg *lnwire.ZkPayRevoke, p lnpeer.Peer) {
+	zkchLog.Info("Just received ZkPayRevoke with length: ", len(msg.RevokeToken))
+
+	// TEMPORARY dummy message
+	payTokenBytes := []byte{'d', 'u', 'm', 'm', 'y'}
+
+	zkPayToken := lnwire.ZkPayToken{
+		PayToken: payTokenBytes,
+	}
+	p.SendMessage(false, &zkPayToken)
+}
+
+func (z *zkChannelManager) processZkPayToken(msg *lnwire.ZkPayToken, p lnpeer.Peer) {
+	zkchLog.Info("Just received ZkPayToken with length: ", len(msg.PayToken))
 }
