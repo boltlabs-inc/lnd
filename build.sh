7c7660d7e07211f045f0bf91a69ec0032ccf52c9
Darius Parvin <darius@berkeley.edu>
2020-02-06 15:25:07 -0800

    add build.sh for building libzkchannels

---
+++ build.sh
@@ -0,0 +1,10 @@
+cd libzkchannels
+git pull
+. ./env
+make mpcgotest
+cargo build --release --manifest-path ./Cargo.toml --features mpc-bitcoin
+export CGO_LDFLAGS="-L$(pwd)/target/release"
+export LD_LIBRARY_PATH="$(pwd)/target/release"
+cd ..
+go get github.com/boltlabs-inc/libzkchannels
+go test -v github.com/boltlabs-inc/libzkchannels
