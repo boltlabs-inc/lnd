745b243120dd0a918c8f20d69610cf330c4ac051
J. Ayo Akinyele <ayo@boltlabs.io>
2020-05-06 01:30:55 -0400

    add libzkchannels build to travis config

---
+++ scripts/install_libzkchannels.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+LND_PATH=$1
+FOUND_LND=0
+
+if [[ $LND_PATH = "" ]]; then
+   echo "Missing path to zklnd..."
+fi
+
+if [ -d "$LND_PATH/make" ]; then
+   echo "Will output config to $LND_PATH/make"
+   FOUND_LND=1
+else
+   echo "Did not specify correct path to zklnd..."
+fi
+
+git clone https://github.com/boltlabs-inc/libzkchannels.git
+
+cd libzkchannels/
+
+unset ZK_DEPS_INSTALL
+
+ROOT=$(pwd)
+ZK_DEPS_INSTALL=${ROOT}/deps/root
+
+set -e
+export ZK_DEPS_INSTALL
+export LD_LIBRARY_PATH=${ZK_DEPS_INSTALL}/lib:${LD_LIBRARY_PATH}
+export PATH=$ZK_DEPS_INSTALL/bin:$PATH
+
+make distclean
+./deps/install_packages.sh
+sudo dpkg -i ./deps/emp-sh2pc/libcrypto++9v5*.deb
+sudo dpkg -i ./deps/emp-sh2pc/libcrypto++-dev*.deb
+make deps
+redis-server --daemonize yes
+redis-cli ping
+set +e
+
+cargo build --release --manifest-path ./Cargo.toml
+
+echo "export CGO_LDFLAGS=\"-L$(pwd)/target/release\"" > libzkchannels.mk
+echo "export LD_LIBRARY_PATH=\"$(pwd)/target/release\"" >> libzkchannels.mk
+
+if [ $FOUND_LND -eq 1 ]; then
+   echo "Save libzkchannels build config to $LND_PATH/make"
+   cp -v libzkchannels.mk $LND_PATH/make/
+fi
+cd ..
