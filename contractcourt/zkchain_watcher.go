8bc27b659a871b3663caea59c2afa68f13f59ef4
Darius Parvin <darius@berkeley.edu>
2020-08-07 06:33:43 -0700

    mark channels as ConfirmedClosed

--- contractcourt/zkchain_watcher.go
+++ contractcourt/zkchain_watcher.go
@@ -346,8 +346,6 @@ func (c *zkChainWatcher) zkCloseObserver(spendNtfn *chainntnfs.SpendEvent) {
 					inputTxid, err)
 			}
 
-			// TODO ZKLND-39 - Mark channel as closed. For merch and cust
-
 		case isMerchCloseTx && isMerch:
 			log.Debug("Merch-close-tx detected")
 
